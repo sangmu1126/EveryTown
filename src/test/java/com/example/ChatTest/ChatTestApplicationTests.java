4d4ea0ed1d615ab5efff19d8a176437ec9372728
sangmu1126@gmail.com <sangmu1126@gmail.com>
2023-11-07 02:26:53 +0900

    first upload

---
+++ src/test/java/com/example/ChatTest/ChatTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChatTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
