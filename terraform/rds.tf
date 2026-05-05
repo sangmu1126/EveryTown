38f6ad4e17309f7b79042674265fe958690f67b2
sangmu1126@gmail.com <sangmu1126@gmail.com>
2026-05-06 00:11:41 +0900

    provision mysql rds instance within private subnets

---
+++ terraform/rds.tf
@@ -0,0 +1,19 @@
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.project_name}-db-subnet-group"
+  subnet_ids = aws_subnet.private[*].id
+}
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  db_name              = "everytowndb"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql8.0"
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  skip_final_snapshot  = true
+  multi_az             = true
+}
